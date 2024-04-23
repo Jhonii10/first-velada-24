3da05c425b42f9202e8c54cce22d353c5e81cb36
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-23 14:42:28 -0500

    added animate in hero

---
+++ app/sections/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AnimateX from '../components/animateX'
+
+export default function Hero() {
+  return (
+    <div className='flex flex-col items-center text-center'> 
+      <AnimateX/>
+      La primera velada del año<br/>
+      08 de Junio <br/>
+      Finca Manabi<br/>
+    </div>
+  )
+}
