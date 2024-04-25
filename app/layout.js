972a72ba010cacb16c58dcfa00a544028906afac
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-25 16:46:45 -0500

    added pwa

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,23 @@ import "./globals.css";
 const quicksand = Quicksand({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "la 1 velada",
-  description: "la primera velada del año ",
+  title: "la velada",
+  description: "la velada del año ",
+  manifest:"/manifest.json",
+  icons:{
+    apple:"/icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <meta name="theme-color" content="#ffffff"/>
+       </head>
       <body className={quicksand.className}>
       <div className="bg-black text-white font-bold">
       {children}
