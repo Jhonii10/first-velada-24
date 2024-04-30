65d907bc2371958eb3d6c999cdeb202e1c118be9
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-30 15:18:21 -0500

    updated search

--- app/lib/data.js
+++ app/lib/data.js
@@ -11,12 +11,12 @@ export async function fetchFilteredPlaylist( query, currentPage,) {
   try {
     const playlist = await sql`
       SELECT
-        playlist.id,
+        playlist.id,  
         playlist.titulo,
         playlist.artista,
         playlist.genero
       FROM playlist
-      WHERE playlist.titulo LIKE ${`%${query}%`}  -- Assuming you're filtering by title
+      WHERE LOWER(playlist.titulo) LIKE LOWER(${'%' + query + '%'}) 
       ORDER BY playlist.id DESC
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
@@ -24,7 +24,7 @@ export async function fetchFilteredPlaylist( query, currentPage,) {
     return playlist.rows;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoices.');
+    throw new Error('Failed to fetch playlist.');
   }
 }
 
@@ -42,6 +42,6 @@ export async function fetchInvoicesPages(query) {
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch total number of invoices.');
+    throw new Error('Failed to fetch total number of playlist.');
   }
 }
